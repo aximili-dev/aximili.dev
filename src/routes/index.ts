88903e5466e0434923c0d85e45b38e0c948a4cd7
Daniel Litvak <dlitvak98@protonmail.com>
2022-01-17 22:47:44 +0100

    Set up contact page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,15 @@ import { Router } from 'express';
 const router = Router();
 
 router.get(/^\/$/, (req, res) => {
-  res.render('pages/index');
+  res.render('pages/index', { currentPage: 'index' });
+});
+
+router.get(/^\/about$/, (req, res) => {
+  res.render('pages/wip', { currentPage: 'about' });
+});
+
+router.get(/^\/contact$/, (req, res) => {
+  res.render('pages/contact', { currentPage: 'contact' });
 });
 
 export default router;
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 
 import blogRouter from './blog';
+import projectRouter from './projects';
 
 import indexRouter from './index';
+
 
 const router = Router();
 
 router.use('/', indexRouter);
 router.use('/blog', blogRouter);
+router.use('/projects', projectRouter);
 
 export default router;
