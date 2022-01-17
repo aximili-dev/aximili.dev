cc809bb70a0f6118054c12b26b7480aeeb98862e
Daniel Litvak <dlitvak98@protonmail.com>
2022-01-17 19:43:21 +0100

    Change some styles

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,28 @@
+import path from 'path';
+
 import { json } from 'body-parser';
 import cors from 'cors';
-import express, { NextFunction, Request, Response } from 'express';
+import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import { HttpError, NotFound } from 'http-errors';
+import morgan from 'morgan';
+import { http } from 'winston';
 
 import indexRouter from './routes/root';
+import { createLogger } from './util/logger';
 
 const app = express();
+
+const httpLogger = createLogger('http', 'dev');
+
+if (process.env.NODE_ENV === 'prod')
+  app.use(morgan('combined', { stream: { write: (str) => httpLogger.info(str.trim()) } }));
+else
+  app.use(morgan('dev', { stream: { write: (str) => httpLogger.info(str.trim()) } }));
 
 // Set up middlewares
 app.use(json());
 app.use(cors());
-app.use(express.static('../static'));
+app.use(express.static(path.join(__dirname, '..', 'static')));
 
 // Set up template engine
 app.set('view engine', 'pug');
@@ -24,7 +36,7 @@ app.use((req, res, next) => {
 });
 
 // Error handler
-app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
+const errorHandler: ErrorRequestHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
   let errorCode: number;
   let errorMessage: string;
 
@@ -36,7 +48,12 @@ app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
     errorMessage = error.message;
   }
 
-  res.render('pages/error', { errorCode, errorMessage });
-});
+  if (res.headersSent)
+    next(error);
+  else
+    res.render('pages/error', { errorCode, errorMessage });
+};
+
+app.use(errorHandler);
 
 export default app;
