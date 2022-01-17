0820044e4589a8e63c8d41e47e48ffc27c26f2dd
Daniel Litvak <dlitvak98@protonmail.com>
2022-01-17 17:57:38 +0100

    Set up basic pages and styles

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,42 @@
 import { json } from 'body-parser';
 import cors from 'cors';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { HttpError, NotFound } from 'http-errors';
 
-import indexRouter from './routes/index';
+import indexRouter from './routes/root';
 
 const app = express();
 
 // Set up middlewares
 app.use(json());
 app.use(cors());
+app.use(express.static('../static'));
 
 // Set up template engine
 app.set('view engine', 'pug');
 
 // Set up routes
 app.use(indexRouter);
+
+// 404 Handler
+app.use((req, res, next) => {
+  next(new NotFound());
+});
+
+// Error handler
+app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
+  let errorCode: number;
+  let errorMessage: string;
+
+  if (error instanceof HttpError) {
+    errorCode = error.status;
+    errorMessage = error.message;
+  } else {
+    errorCode = 500;
+    errorMessage = error.message;
+  }
+
+  res.render('pages/error', { errorCode, errorMessage });
+});
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get(/\/(index)?/, (req, res) => {
-  res.render('index');
+router.get(/^\/$/, (req, res) => {
+  res.render('pages/index');
 });
 
 export default router;
